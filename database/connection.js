681d94fa36430d3276f7e475ae4e9ec25b80755a
Indrjeet <99714911+indrjeet8singh@users.noreply.github.com>
2024-09-08 20:30:32 +0530

    connection.js
    secure connection

--- database/connection.js
+++ database/connection.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 
-const password = encodeURIComponent(process.env.PASSWORD);
-const db =`mongodb+srv://indrjeet8singh:${password}@cluster0.wybmel9.mongodb.net/`;
+// const password = encodeURIComponent(process.env.PASSWORD);
+// const db =`mongodb+srv://indrjeet8singh:${password}@cluster0.wybmel9.mongodb.net/`;
 
+const db = process.env.DATABASE;
 
 mongoose.connect(db).then(()=>{
     console.log("database connected...");
