3b1e96df0f9e906bd5863c574ef14488aa19f445
indrjeet8singh <indrjeet8singh@gmail.com>
2024-09-08 20:56:55 +0530

    Aapka commit message yahan

--- database/connection.js
+++ database/connection.js
@@ -1,8 +1,17 @@
+// const mongoose = require('mongoose');
+
+// const password = encodeURIComponent(process.env.PASSWORD);
+// const db =`mongodb+srv://indrjeet8singh:${password}@cluster0.wybmel9.mongodb.net/`;
+
+
+// mongoose.connect(db).then(()=>{
+//     console.log("database connected...");
+// })
 const mongoose = require('mongoose');
 
-const password = encodeURIComponent(process.env.PASSWORD);
-const db =`mongodb+srv://indrjeet8singh:${password}@cluster0.wybmel9.mongodb.net/`;
 
+
+const db = process.env.DATABASE;
 
 mongoose.connect(db).then(()=>{
     console.log("database connected...");
